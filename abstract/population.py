b5193436c83b87a25563f2387fd9923c61f11291
Arthur <drabazha.arthur@gmail.com>
2020-09-15 21:25:08 +0300

    Rearange properties from child to base class

--- abstract/population.py
+++ abstract/population.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 import abc
 
 from abstract.fitness import FitnessFunction
@@ -9,7 +10,11 @@ class Chromosome(abc.ABC):
     Class represents a single unit of genetic population.
     """
 
-    @abc.abstractmethod
+    def __init__(self, phenotype, pop, fs):
+        self.__phenotype = phenotype
+        self.__population_value = pop
+        self.__fitness_score = fs
+
     def has_len(self, _len: int) -> bool:
         """
         Function to compare actual
@@ -17,35 +22,40 @@ class Chromosome(abc.ABC):
         :param _len:
         :return: Bool
         """
-        pass
+        return _len == len(self.population_val)
 
-    @abc.abstractmethod
-    def phenotype(self, value=None):
-        """
-        Phenotype getter and setter.\n
-        Normal representation of chromosome.
-        :return:
-        """
-        return
+    @property
+    def phenotype(self):
+        return self.__phenotype
 
-    @abc.abstractmethod
-    def population_val(self, value=None):
-        """
-        Population getter and setter.\n
-        Binary representation of chromosome.
-        :param value:
-        :return:
-        """
-        pass
+    @phenotype.setter
+    def phenotype(self, value):
+        self._phenotype_setter(value)
 
-    @abc.abstractmethod
-    def fitness_score(self, value=None):
-        """
-        Fitness score getter and setter.
-        :param value:
-        :return:
-        """
-        return
+    def _phenotype_setter(self, value):
+        self.__phenotype = value
+
+    @property
+    def population_val(self):
+        return self.__population_value
+
+    @population_val.setter
+    def population_val(self, value):
+        self._pop_val_setter(value)
+
+    def _pop_val_setter(self, value):
+        self.__population_value = value
+
+    @property
+    def fitness_score(self):
+        return self.__fitness_score
+
+    @fitness_score.setter
+    def fitness_score(self, value):
+        self._fitness_score_setter(value)
+
+    def _fitness_score_setter(self, value):
+        self.__fitness_score = value
 
 
 class IPopulation(abc.ABC):
@@ -54,6 +64,16 @@ class IPopulation(abc.ABC):
     Class contains data which describes pop units as group\n
     Also validation, appliers functions can be written there
     """
+    def __init__(self, input_set: List[Chromosome]):
+
+        random_unit_length = len(random.choice(input_set)
+                                 .population_val)
+
+        if not all(unit.has_len(random_unit_length) for unit in input_set):
+            raise ValueError("Population elements should be equal in their size.")
+
+        self.__population = input_set
+        self.__unit_length = random_unit_length
 
     @abc.abstractmethod
     def apply_fitness(self, func: FitnessFunction):
@@ -61,23 +81,6 @@ class IPopulation(abc.ABC):
         Method used to apply fitness function to whole population
         :param func:
         :return:
-        """
-        pass
-
-    @abc.abstractmethod
-    def unit_length(self, value=None):
-        """
-        Getter and setter for each population units' length
-        :param value:
-        :return:
-        """
-        pass
-
-    @abc.abstractmethod
-    def population(self, value: List[Chromosome] = None) \
-            -> List[Chromosome]:
-        """
-        Method used to set and retrieve population list
         """
         pass
 
@@ -96,3 +99,27 @@ class IPopulation(abc.ABC):
                  value is second's.
         """
         pass
+
+    @property
+    def unit_length(self):
+        """
+        Getter for each population units' length
+        :param value:
+        :return:
+        """
+        return self.__unit_length
+
+    @unit_length.setter
+    def unit_length(self, value: int):
+        """
+        Method used to set population list
+        """
+        self.__unit_length = value
+
+    @property
+    def population(self):
+        return self.__population
+
+    @population.setter
+    def population(self, value):
+        self.__population = value
--- concrete/population.py
+++ concrete/population.py
@@ -17,77 +17,49 @@ class BinaryChromosome(Chromosome):
 
             self.__phenotype = value
         else:
-            self.__phenotype = random.uniform(a, b)
+            self.__phenotype = random.randint(a, b)
 
-        self.__population_G = binary.to_gray(bin(self.__phenotype))
+        self.__population_G = binary.to_gray(bin(self.__phenotype),
+                                             # If we translate maximal possible value to binary format
+                                             # then we can get length of chromosome population.
+                                             len(bin(b)[2:]))
         self.__fitness_score: float = 0.
+        super().__init__(self.__phenotype, self.__population_G, self.__fitness_score)
 
-    def has_len(self, _len: int) -> bool:
-        return _len == len(self.population_val())
+    def _pop_val_setter(self, value):
+        self.__population_G = value
+        self.__phenotype = int(binary.from_gray(self.__population_G), 2)
 
-    def phenotype(self, value=None):
-        if value is not None:
-            self.__phenotype = value
-        else:
-            return self.__phenotype
-
-    def population_val(self, value=None):
-        if value is not None:
-            self.__population_G = value
-        else:
-            return self.__population_G
-
-    def fitness_score(self, value=None):
-        if value is not None:
-            self.__fitness_score = value
-        else:
-            return self.__fitness_score
-    
 
 class BinaryPopulation(IPopulation):
 
     def __init__(self, input_set: List[Chromosome]):
+        super().__init__(input_set)
 
-        random_unit_length = len(random.choice(input_set)
-                                 .population_val())
+    def apply_fitness(self, func: FitnessFunction):
 
-        if not all(unit.has_len(random_unit_length) for unit in input_set):
-            raise ValueError("Population elements should be equal in their size.")
+        for unit in self.population:
+            unit.fitness_score = func.fitness(unit.phenotype)
 
-        self.__population = input_set
-        self.__unit_length = random_unit_length
+    def get_fittest(self, count: int = 1) -> List[Chromosome]:
 
-    def unit_length(self, value=None):
-        if value is not None:
-            self.__unit_length = value
-        else:
-            return self.__unit_length
-
-    def population(self, value=None):
-        if value is not None:
-            self.__population = value
-        else:
-            return self.__population
-
-    def get_fittest(self, count: int) -> List[Chromosome]:
-
-        return sorted(self.__population,
+        return sorted(self.population,
                       key=lambda unit: unit.fitness_score,
                       reverse=True)[:count]
 
     def get_pairs(self) -> dict:
-        ids = [i for i in range(len(self.__population))]
+        ids = [i for i in range(len(self.population))]
         pairs = {}
 
         while len(ids) != 0:
-            k = ids.pop(random.choice(ids))
-            v = ids.pop(random.choice(ids))
+            k = random.choice(ids)
+            ids.remove(k)
+
+            v = random.choice(ids)
+            ids.remove(v)
+
             pairs[k] = v
 
         return pairs
 
-    def apply_fitness(self, func: FitnessFunction):
-
-        for unit in self.__population:
-            unit.fitness_score = func.fitness(unit.phenotype())
 
